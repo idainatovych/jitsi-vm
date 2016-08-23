696ad47f0cdf8f84df6212a233f6d4b45ee4f2b4
Illia Daynatovich <idaynatovich@atlassian.com>
2016-08-23 13:25:46 +0300

    Set up repo

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Variables
+CURRENT_DIR=`pwd`
+VM_URL="devvm.jitsi.meet"
+VM_IP="192.168.88.88"
+SUCCESS="Your Dev VM is now accessible from https://devvm.jitsi.meet!"
+ALIAS="$VM_IP  $VM_URL"
+
+printf "=============== Cloning Git repos ===============\n"
+cd ../
+
+echo "Cloning lib-jitsi-meet"
+git clone git@github.com:jitsi/lib-jitsi-meet.git
+echo "Cloning jitsi-meet"
+git clone git@github.com:jitsi/jitsi-meet.git
+
+printf "\n\n=============== Set up Dev VM ===============\n\n"
+cd $CURRENT_DIR
+vagrant up
+
+printf "\n=============== Making alias for Dev VM ===============\n"
+echo "Check if the alias exist"
+
+CONDITION=`grep $VM_URL /etc/hosts`
+if [ $? -eq 1 ]
+then
+  echo "Creating the alias"
+  echo $ALIAS >> /etc/hosts && echo $SUCCESS
+else
+  echo "Alias exist"
+fi
+
+printf "\n=============== Done ===============\n"
+printf "Done! Run vagrant build to build the project\n"
---
+++ vm-provision.sh
@@ -0,0 +1,22 @@
+echo "Installing prerequisites"
+
+sudo apt-get update
+sudo apt-get install -y git npm nodejs-legacy
+sudo npm -g static-server
+
+echo "Go to working directory"
+cd /jitsi-meet
+
+echo "Link lib-jitsi-meet dependency to local copy"
+cd /lib-jitsi-meet
+npm link
+cd /jitsi-meet
+npm link lib-jitsi-meet
+
+echo "Installing dependencies"
+npm install --force
+
+#echo "Building the project"
+#cd /jitsi-meet
+#make
+#sudo static-server --port 80
